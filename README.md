843df87fe5656ca221527b246e30a68fa1801de0
Alex Davidson <alexdavidson4@gmail.com>
2022-11-09 11:21:26 -0500

    Very small tweaks. Mostly just making sure my github works

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 3501_Project
 Project with peopel
+
+Going to add some tweaks to the location file.  Just comment in your personal location when you update
--- path_config.h
+++ path_config.h
@@ -1,3 +1,4 @@
-#define MATERIAL_DIRECTORY "E:\\TextureDemo"
+//#define MATERIAL_DIRECTORY "E:\\TextureDemo"
+#define MATERIAL_DIRECTORY "C:/Users/alexd/source/repos/3501_Project"
 //#define MATERIAL_DIRECTORY "C:\\Users\\jyrou\\Downloads\\TextureDemo\\TextureDemo"
 // change to specify your own location here
