c9ab86ef91dbef3f7d3bd98858c5e9b878806ee8
Sherewood <52391353+Sherewood@users.noreply.github.com>
2022-11-21 10:03:43 -0500

    jkljjl

--- path_config.h
+++ path_config.h
@@ -1,4 +1,4 @@
 //#define MATERIAL_DIRECTORY "E:\\TextureDemo"
-#define MATERIAL_DIRECTORY "C:/Users/alexd/source/repos/3501_Project"
-//#define MATERIAL_DIRECTORY "C:\\Users\\jyrou\\Downloads\\TextureDemo\\TextureDemo"
+//#define MATERIAL_DIRECTORY "C:/Users/alexd/source/repos/3501_Project"
+#define MATERIAL_DIRECTORY "C:\\Users\\jyrou\\OneDrive\\Email attachments\\Documents\\GitHub"
 // change to specify your own location here
